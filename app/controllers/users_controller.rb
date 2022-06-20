cce5d9588843fbbd29f0976e4b61dbefd48d1246
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-06-20 03:39:04 +0000

    [Add] 4-25 自己紹介

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
